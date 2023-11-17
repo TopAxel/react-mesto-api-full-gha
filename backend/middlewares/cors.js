b9e4dab519762e985a935211cfd05f2b0792b270
Artem Popov <Top-axel@yandex.ru>
2023-11-17 17:29:05 +0300

    ПР15.6

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,20 +5,21 @@ const allowedCors = [
   'http://api.artempopov.nomoredomainsrocks.ru',
   'http://localhost:3000',
 ];
-const ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
     return res.end();
   }
 
